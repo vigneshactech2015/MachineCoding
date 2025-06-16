4e4af3e0308bf73290353fefa83fa1c408fcd7b4
Vignesh Vijayakumar <85957001+vigneshactech2015@users.noreply.github.com>
2025-06-16 10:52:28 +0530

    Update 6.Popup.js

--- 6.Popup.js
+++ 6.Popup.js
@@ -6,7 +6,13 @@ function Popup() {
     <div >
     <button onClick={()=>setShow(true)}>Show Popup</button>
 
-    {show && <div style={{width:"100vw",height:"100vh",backgroundColor:"rgba(49,49,49,0.8)",position:"fixed",top:"0",left:"0",right:"0",bottom:"0"}} onClick={()=>setShow(false)}> <div onClick={(e)=>e.stopPropagation()} style={{position:"absolute",top:"40%",left:"50%",transform:"translate(-50%,-50%)",lineHeight:"1.4",background:"white",padding:"14px 28px",borderRadius:"3px",maxWidth:"600px",minWidth:"300px"}}>
+    {show && 
+<div style={{width:"100vw",height:"100vh",backgroundColor:"rgba(49,49,49,0.8)",
+      position:"fixed",top:"0",left:"0",right:"0",bottom:"0"}} 
+           onClick={()=>setShow(false)}> 
+<div onClick={(e)=>e.stopPropagation()} 
+style={{position:"absolute",top:"40%",left:"50%",transform:"translate(-50%,-50%)",lineHeight:"1.4",
+background:"white",padding:"14px 28px",borderRadius:"3px",maxWidth:"600px",minWidth:"300px"}}>
         </div>
         </div>}
     </div>
